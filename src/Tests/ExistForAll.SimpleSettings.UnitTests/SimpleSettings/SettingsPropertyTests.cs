b1156bd4a9f100db951a315f894950ed4007d9ea
guy-lud <guy.lud@gmail.com>
2026-07-15 16:55:21 +0300

    test(04-05): add failing AllowEmpty=false empty/whitespace rejection tests
    - AllowEmpty=false rejects "" and whitespace via SettingsPropertyNullException (value-free)
    - AllowEmpty=true still binds "" and whitespace as-is

--- src/Tests/ExistForAll.SimpleSettings.UnitTests/SimpleSettings/SettingsPropertyTests.cs
+++ src/Tests/ExistForAll.SimpleSettings.UnitTests/SimpleSettings/SettingsPropertyTests.cs
@@ -1,3 +1,5 @@
+using ExistForAll.SimpleSettings.Binder;
+
 namespace ExistForAll.SimpleSettings.UnitTests.SimpleSettings
 {
 	public class SettingsPropertyTests
@@ -15,10 +17,80 @@ namespace ExistForAll.SimpleSettings.UnitTests.SimpleSettings
 			await Assert.That(exception!.Message.Contains(nameof(IWithNonNullInterface.Value))).IsTrue();
 		}
 
+		[Test]
+		public async Task Build_WhenAllowEmptyIsFalse_EmptyString_ShouldThrowNullException()
+		{
+			var sut = BuildWith(nameof(IRejectsEmpty.Value), "");
+
+			await Assert.That(() => sut.GetSettings<IRejectsEmpty>())
+				.Throws<SettingsPropertyNullException>();
+		}
+
+		[Test]
+		public async Task Build_WhenAllowEmptyIsFalse_WhitespaceString_ShouldThrowNullException()
+		{
+			var sut = BuildWith(nameof(IRejectsEmpty.Value), "   ");
+
+			await Assert.That(() => sut.GetSettings<IRejectsEmpty>())
+				.Throws<SettingsPropertyNullException>();
+		}
+
+		[Test]
+		public async Task Build_WhenAllowEmptyIsFalse_EmptyString_MessageIsValueFreeWithPropertyName()
+		{
+			var sut = BuildWith(nameof(IRejectsEmpty.Value), "   ");
+
+			var exception = await Assert.That(() => sut.GetSettings<IRejectsEmpty>())
+				.Throws<SettingsPropertyNullException>();
+
+			await Assert.That(exception!.Message.Contains(nameof(IRejectsEmpty.Value))).IsTrue();
+			await Assert.That(exception!.Message.Contains("   ")).IsFalse();
+		}
+
+		[Test]
+		public async Task Build_WhenAllowEmptyIsTrue_EmptyString_BindsValue()
+		{
+			var sut = BuildWith(nameof(IAllowsEmpty.Value), "");
+
+			var settings = sut.GetSettings<IAllowsEmpty>();
+
+			await Assert.That(settings.Value).IsEqualTo("");
+		}
+
+		[Test]
+		public async Task Build_WhenAllowEmptyIsTrue_WhitespaceString_BindsValue()
+		{
+			var sut = BuildWith(nameof(IAllowsEmpty.Value), "   ");
+
+			var settings = sut.GetSettings<IAllowsEmpty>();
+
+			await Assert.That(settings.Value).IsEqualTo("   ");
+		}
+
+		private static SettingsBuilder BuildWith(string key, string value)
+		{
+			var collection = new InMemoryCollection();
+			collection.Add("RejectsEmpty", key, value);
+			collection.Add("AllowsEmpty", key, value);
+
+			return SettingsBuilder.CreateBuilder(x => x.AddSectionBinder(new InMemoryBinder(collection)));
+		}
+
 		public interface IWithNonNullInterface
 		{
 			[SettingsProperty(AllowEmpty = false)]
 			int Value { get; set; }
+		}
+
+		public interface IRejectsEmpty
+		{
+			[SettingsProperty(AllowEmpty = false)]
+			string Value { get; set; }
+		}
+
+		public interface IAllowsEmpty
+		{
+			string Value { get; set; }
 		}
 	}
 }
